c0b897d87b3857d8e1bee4164dc5d96c35166b4f
Mark S <mdsoren3@gmail.com>
2013-10-30 11:05:10 -0500

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -37,6 +37,21 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      
+      describe "after submission" do
+        before { click_button submit }
+        
+        it { should have_selector('title', text: User.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
         
   end
